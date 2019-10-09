407b0376ebd3f7deaf1f34d6a3266747fc4d3e6b
Szabo <aszabo@mcw.edu>
2019-10-09 17:11:08 -0500

    Boric acid data example

--- SPreg_develop.R
+++ SPreg_develop.R
@@ -38,7 +38,34 @@ b <- sprr(cbind(NResp, ClusterSize-NResp) ~ Trt+log(ClusterSize),
                 data=egde, weights=egde$Freq,
                 control=list(eps=0.01,maxit=100))
 
+#
+ba <- read.CBData("z:/EOGeorge/Data/Binary/BoricAcidMousedata_processed.csv",
+                  sep=",", skip=1)
+ba.mod <- sprr(cbind(NResp, ClusterSize-NResp) ~ Trt,
+          data=ba, weights=ba$Freq,link="log",
+          control=list(eps=0.01,maxit=100))
 
+
+ba.mod1 <- sprr(cbind(NResp, ClusterSize-NResp) ~ Trt + log(ClusterSize),
+               data=ba, weights=ba$Freq, link="log",
+               control=list(eps=0.01,maxit=100),
+               start=list(q=ba.mod$q, beta=c(coef(ba.mod), 0),
+                          mu1=lambda_from_p(ba.mod$q)[2]))
+ba.mod2 <- sprr(cbind(ClusterSize-NResp, NResp) ~ Trt ,
+                data=ba, weights=ba$Freq, link="log",
+                control=list(eps=0.01,maxit=100))
+
+zz <- mc.est(ba)
+ba_zz <- merge(ba, zz, by=c("Trt","ClusterSize","NResp"), all.x = TRUE,
+               all.y=FALSE)
+with(ba_zz, sum(Freq * log(Prob)))
+
+nd <- data.frame(Trt = unique(ba$Trt))
+nd$p <- predict(ba.mod, newdata=nd)
+nd
+
+l <- predict(ba.mod, newdata=nd, newn=15, type="lvec")
+matplot(t(l), type="l")
 
 #########
 
